501ddb4c979eb031f394c1bba346cfa086c19f51
SimoneRicco <smnrcc0@gmail.com>
2023-05-18 20:08:08 +0200

    fix first part

--- my-app/src/components/DcProducts.vue
+++ my-app/src/components/DcProducts.vue
@@ -6,50 +6,50 @@ export default {
 
 <template>
     <div class="product-list">
-        <div class="container myList">
-            <div class="col1">
-                <ul>
-                    <li class="list-title">DC Comics</li>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>Tv</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
-                </ul>
-                <ul>
-                    <li class="list-title">Shop</li>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
-                </ul>
-            </div>
-            <div class="col2">
-                <ul>
-                    <li class="list-title">DC</li>
-                    <li>Terms Of Use</li>
-                    <li>Privacy polici (New)</li>
-                    <li>Ad Choices</li>
-                    <li>Advertising</li>
-                    <li>Jobs</li>
-                    <li>Subscriptions</li>
-                    <li>Talent Workshops</li>
-                    <li>CPSC Certificates</li>
-                    <li>Ratings</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-            <div class="col3">
-                <ul>
-                    <li class="list-title">Sites</li>
-                    <li>DC</li>
-                    <li>MAD Magazine</li>
-                    <li>DC Universe</li>
-                    <li>DC Power Visa</li>
-                </ul>
-            </div>
-        </div>
         <div class="container">
+            <div class="myList">
+                <div class="col1">
+                    <ul>
+                        <li class="list-title">DC Comics</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li class="list-title">Shop</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="col2">
+                    <ul>
+                        <li class="list-title">DC</li>
+                        <li>Terms Of Use</li>
+                        <li>Privacy polici (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col3">
+                    <ul>
+                        <li class="list-title">Sites</li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
             <div class="background-image">
                 <img src="../../img/dc-logo-bg.png" alt="">
             </div>
@@ -58,6 +58,10 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: space-between;
+}
 ul{
     list-style: none;
     margin-bottom: 1rem;
