e1e2f37ea62d450af09a6dc0b2c29ed1614c0dfc
SimoneRicco <smnrcc0@gmail.com>
2023-05-19 18:41:07 +0200

    add cards

---
+++ my-app/src/components/Card.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        imgLink: String,
+        price: String,
+        series: String,
+        tyoe: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="image">
+            <img :src="imgLink" alt="Card">
+        </div>
+        <div class="title">{{ series }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .image{
+        width: 100px;
+        height: 100px;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+        
+    }
+    .card{
+        color: white;
+        word-wrap: break-word;
+    }
+</style>
